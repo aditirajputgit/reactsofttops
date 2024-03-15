cd6914a6a0b32a44606952a556aba563ff00f6ee
Aditi Rajput <aditirajput200017@gmail.com>
2024-03-15 19:03:43 +0530

    student

--- learning/src/component/Student.tsx
+++ learning/src/component/Student.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Typography } from "@material-ui/core";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
+import { styled } from "@material-ui/styles";
 import React, { Component } from "react";
 
 interface Props {
@@ -18,32 +19,35 @@ export default class Student extends Component<Props, S> {
   render() {
     return (
       <>
-        <Box style={{ textAlign: "center" }}>
-          <Typography variant="h2">Hello {this.props.name}</Typography>
-          {this.state.show ? this.props.name : null}
-          <Button
-            onClick={() => {
-              this.setState({ show: false });
-            }}
-          >
-            Hide
-          </Button>
-          <Button
-            onClick={() => {
-              this.setState({ show: true });
-            }}
-          >
-            Show
-          </Button>
-          <Button
-            onClick={() => {
-              this.setState({ show: !this.state.show });
-            }}
-          >
-            Toggle
-          </Button>
-        </Box>
+        <OuterBoxStyled style={{ textAlign: "center" }}>
+          <Typography>heloo</Typography>
+          <form>
+            <TextField
+              required
+              id="standard-required"
+              label=""
+              variant="outlined"
+              className="textFieldInput"
+              //   inputProps={{
+              //     style: {
+              //       height: "1px",
+              //       backgroundColor: "red",
+              //     },
+              //   }}
+            />
+          </form>
+          <Box>
+            <Button>Red</Button>
+            <Button>Blue</Button>
+          </Box>
+        </OuterBoxStyled>
       </>
     );
   }
 }
+
+const OuterBoxStyled = styled(Box)({
+  "& .textFieldInput": {
+    height: "20px",
+  },
+});
